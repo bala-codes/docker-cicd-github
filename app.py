aec77c8385ecc33679ade0f02e9db59b7d626a71
Balakrishna Kumar V <balakrishnakumar.v@gmail.com>
2022-04-25 13:31:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    response = "Hello World. Docker - Github Actions - AWS ECR - CICD Pipeline !!!!!"
+    response = "Hello World. Docker - Github Actions - AWS ECR - AWS Lambda - CICD Pipeline !!!!!"
     return {
         'statusCode': 200,
         'body': json.dumps(response)
